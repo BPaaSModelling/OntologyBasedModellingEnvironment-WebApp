12c3333f6c4825e9f00230b8cb16516286c029b0
charutapande <32811202+charutapande@users.noreply.github.com>
2017-10-24 17:53:18 +0200

    Rectangles joined by a line

--- src/app/modelling-area/modelling-area.component.ts
+++ src/app/modelling-area/modelling-area.component.ts
@@ -20,15 +20,23 @@ export class ModellingAreaComponent implements OnInit {
   private canvas;
   private boundBox;
   private shape;
+  private line;
+  private group;
+
+  private cnt = 0;
 
   constructor() { }
 
   ngOnInit() {
     console.log(fabric.version);
 
-    this.canvas = new fabric.Canvas('canvas',{
-
+    this.canvas = new fabric.Canvas('canvas', {
     });
+
+    /*const activeObject = this.canvas.getActiveObject();
+    if (activeObject) {
+        this.canvas.remove(activeObject);
+    }*/
   }
 
   ngOnChanges(changes: SimpleChanges) {
@@ -36,14 +44,51 @@ export class ModellingAreaComponent implements OnInit {
   }
 
   printNewElement(element: MetamodelElementModel): void {
+
     if (element !== undefined) {
       this.shape = new fabric.Rect({
         width: 200,
-        height: 200,
-        fill: 'red'
+        height: 150,
+        fill: 'white',
+        stroke: 'black'
       });
+      this.shape.id = 'id' + this.cnt;
+      this.cnt++;
       this.canvas.add(this.shape);
+
+      this.canvas.getObjects().forEach(function (o) {
+        console.log(o.id);
+      });
+
+      if (this.canvas.getObjects().length === 2) {
+        const sourceObj = this.canvas.item(0);
+        console.log('Got id:' + sourceObj.id);
+        const targetObj = this.canvas.item(1);
+        console.log('Got id:' + targetObj.id);
+
+        console.log(sourceObj);
+        console.log(targetObj);
+
+        const leftX = targetObj.aCoords.br.x;
+        const leftY = targetObj.aCoords.tr.y + (targetObj.height) / 2;
+
+        const rightX = sourceObj.aCoords.bl.x;
+        const rightY = sourceObj.aCoords.tl.y + (sourceObj.height) / 2;
+
+        console.log(leftX + ' ' + leftY + ' ' + rightX + ' ' + rightY);
+        this.line = new fabric.Line([leftX, leftY, rightX, rightY], {
+          stroke: 'black'
+        });
+        this.canvas.add(this.line);
+
+        /*this.canvas.remove(sourceObj);
+        this.canvas.remove(targetObj);
+        this.group = new fabric.Group([sourceObj, targetObj, this.line], {});
+        this.canvas.add(this.group);*/
+
+      }
     }
 
   }
+
 }
