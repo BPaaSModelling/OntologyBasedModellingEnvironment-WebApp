3ef5ccd0105820e481348c32b7e532bb1a1d3015
Stizzo <stefano.izzo@studenti.unicam.it>
2017-11-24 19:53:00 +0100

    added createElement endpoint

--- src/app/_settings/endpoint.settings.ts
+++ src/app/_settings/endpoint.settings.ts
@@ -3,6 +3,7 @@ export class EndpointSettings {
   private static ENDPOINT                   : string = 'http://localhost:8080';
   private static PALETTEELEMENTS            : string = '/ModEnv/getPaletteElements';
   private static PALETTECATEGORIES          : string = '/ModEnv/getPaletteCategories';
+  private static CREATEELEMENT              : string = '/ModEnv/createElement';
 
   public static getPaletteElementsEndpoint(): string {
     return EndpointSettings.ENDPOINT + EndpointSettings.PALETTEELEMENTS;
@@ -10,5 +11,9 @@ export class EndpointSettings {
 
   public static getPaletteCategoriesEndpoint(): string {
     return EndpointSettings.ENDPOINT + EndpointSettings.PALETTECATEGORIES;
+  }
+
+  public static getCreateElementEndpoint(): string {
+    return EndpointSettings.ENDPOINT + EndpointSettings.CREATEELEMENT;
   }
 }
