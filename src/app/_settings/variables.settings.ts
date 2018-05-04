c5f1e9c6e485858106a88fc3975e766c8ed85b7a
manulaur <emanuele.laurenzi@fhnw.ch>
2018-05-04 18:44:32 +0200

    new images

--- src/app/_settings/variables.settings.ts
+++ src/app/_settings/variables.settings.ts
@@ -1,6 +1,6 @@
 export class VariablesSettings{
 
-  public static paletteCategoryConnectorsURI: string = 'http://fhnw.ch/modelingEnvironment/LanguageOntology#Category_Connectors';
+  public static paletteCategoryConnectorsURI: string = 'http://fhnw.ch/modelingEnvironment/PaletteOntology#Category_Connectors';
   public static iconLocation: string = '../assets/images/';
 
 }
