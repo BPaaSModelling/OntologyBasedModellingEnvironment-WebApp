ff2470757f4addb3416f72102d54834e5cc86c7f
Kiril <kirilbuga@yahoo.com>
2024-05-30 19:50:18 +0200

    Moving httpIncterceptor

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,48 +12,4 @@ export const environment = {
     scope: 'openid profile email read:messages',
     redirectUri: window.location.origin
   },
-  httpInterceptor: {
-    allowedList: [
-      // Attach access tokens to any calls to '/api' (exact match)
-      `http:localhost:8080/*`,
-
-      // Attach access tokens to any calls that start with '/api/'
-      '/*',
-
-      // Match anything starting with /api/accounts, but also specify the audience and scope the attached
-      // access token must have
-      {
-        uri: '/api/accounts/*',
-        tokenOptions: {
-          authorizationParams: {
-            audience: 'http://my-api/',
-            scope: 'read:accounts',
-          }
-        },
-      },
-
-      // Matching on HTTP method
-      // {
-      //   uri: '/api/orders',
-      //   httpMethod: 'post',
-      //   tokenOptions: {
-      //     authorizationParams: {
-      //       audience: 'http://my-api/',
-      //       scope: 'write:orders',
-      //     }
-      //   },
-      // },
-
-      // Using an absolute URI
-      // {
-      //   uri: 'https://your-domain.auth0.com/api/v2/users',
-      //   tokenOptions: {
-      //     authorizationParams: {
-      //       audience: 'https://your-domain.com/api/v2/',
-      //       scope: 'read:users',
-      //     }
-      //   },
-      // }
-    ]
-  }
 };
