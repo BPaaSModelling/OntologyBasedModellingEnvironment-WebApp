827da8fc8394c0ed15c0c51134afa201c29916ba
Kiril <kirilbuga@yahoo.com>
2024-05-30 19:48:12 +0200

    Replacing idtoken with an email for simplicity

--- src/app/core/services/auth/http-interceptor.service.ts
+++ src/app/core/services/auth/http-interceptor.service.ts
@@ -8,7 +8,6 @@ import {
 } from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {AuthHttpInterceptor, AuthService, User} from '@auth0/auth0-angular';
-import {UserModel} from '../../../shared/models/User.model';
 import {from} from 'rxjs/internal/observable/from';
 import {catchError, switchMap} from 'rxjs/operators';
 
@@ -24,19 +23,19 @@ export class HttpInterceptorService implements HttpInterceptor {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
     return from(this.authService.getAccessTokenSilently()).pipe(
       switchMap(accessToken =>
-        from(this.authService.getIdTokenClaims().toPromise()).pipe(
-          switchMap(idTokenClaims => {
-            if (accessToken && idTokenClaims) {
-              const userDataBase64 = btoa(JSON.stringify(idTokenClaims));
+        from(this.authService.user$).pipe(
+          switchMap(user => {
+            if (accessToken && user) {
               const clonedRequest = req.clone({
                 headers: req.headers
                   .set('Authorization', `Bearer ${accessToken}`)
-                  .set('X-User-Data', userDataBase64)
+                  .set('X-User-Email', user.email)
               });
 
-              console.log("Headers", clonedRequest.headers.get('X-User-Data'));
+              console.log("Header of the user: ", clonedRequest.headers.get('X-User-Email'));
 
               return next.handle(clonedRequest);
             }
@@ -44,7 +43,7 @@ export class HttpInterceptorService implements HttpInterceptor {
           }),
           catchError(err => {
             // Handle errors if any
-            console.error('Error fetching idTokenClaims', err);
+            console.error('Error fetching user', err);
             return next.handle(req);
           })
         )
