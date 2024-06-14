183b96ad9d8075b8cca47bffd18cdd4fb2fa9bb0
Kiril <kirilbuga@yahoo.com>
2024-06-14 13:38:42 +0200

    Added env vars for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  auth: {
+    domain: 'dev-aoame.eu.auth0.com',
+    clientId: 'hnndSNGQ24vp0LJc9avBqTbEmuaZ9zuz',
+    audience: 'https://aoame-webservice',
+    scope: 'openid profile email read:messages',
+    redirectUri: window.location.origin
+  },
 };
